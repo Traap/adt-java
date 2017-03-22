42602db84796f2ddf1550fbdaf07339d559a7263
Traap <gary.a.howard@mac.com>
2017-03-22 08:56:27 -0700

    Issue #23 - Remove package name com.trapp.samples.

--- src/main/java/Summation.java
+++ src/main/java/Summation.java
@@ -12,8 +12,6 @@
  *  Find the difference between the sum of the squares of the first one hundred
  *  natural numbers and the square of the sum.
  */
-package com.traap.samples;
-
 public class Summation {
 	public static void main(String[] args) {
 		int n = 100;
